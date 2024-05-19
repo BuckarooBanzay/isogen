674e8daa5fd1075335e603ee4a185e08d0872a20
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-05-19 10:41:34 +0200

    draw_cube

--- init.lua
+++ init.lua
@@ -3,9 +3,9 @@ isogen = {}
 local MP = minetest.get_modpath("isogen")
 dofile(MP.."/colors.lua")
 dofile(MP.."/canvas.lua")
-dofile(MP.."/draw.lua")
+dofile(MP.."/draw_cube.lua")
 
 if minetest.get_modpath("mtt") and mtt.enabled then
     dofile(MP.."/colors.spec.lua")
-    dofile(MP.."/draw.spec.lua")
+    dofile(MP.."/draw_cube.spec.lua")
 end
