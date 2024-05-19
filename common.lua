194813a30d2565e4ab54be2926d1f5a5db96908c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-05-19 16:23:47 +0200

    coords fix

--- common.lua
+++ common.lua
@@ -1,35 +1,42 @@
 
 -- size of the resulting image
 function isogen.calculate_image_size(size, cube_len)
-    local width = (size.x * cube_len / 2) + (size.z * cube_len / 2)
-    local height = math.max(
-        cube_len + ((size.x-1) * (cube_len / 4)),
-        cube_len + ((size.z-1) * (cube_len / 4)),
-        cube_len + ((size.y-1) * (cube_len / 2))
-    )
-    -- TODO: wrong calc!
+    local width =
+        (size.x * cube_len / 2) +
+        (size.z * cube_len / 2)
+    local height =
+        (size.x * cube_len / 4) +
+        (size.y * cube_len / 2) +
+        (size.z * cube_len / 4)
 
     return width, height
 end
 
 -- position offset of the center cube (front, bottom)
 -- all other positions are relative to this
-function isogen.get_center_cube_offset(size, cube_len, rotation)
-    local x = (size.x * cube_len / 2) - (cube_len / 2)
-    local max_y = math.max(
-        cube_len + ((size.x-1) * (cube_len / 4)),
-        cube_len + ((size.z-1) * (cube_len / 4)),
-        cube_len + ((size.y-1) * (cube_len / 2))
-    )
+function isogen.get_center_cube_offset(size, cube_len)
+    local x =
+        (size.x * cube_len / 2) -
+        (cube_len / 2)
+    local y =
+        (size.x * cube_len / 4) +
+        (size.y * cube_len / 2) +
+        (size.z * cube_len / 4) -
+        cube_len
 
-    local y = max_y - cube_len
     return x, y
 end
 
 -- returns the cube position offset relative to the center cube
-function isogen.get_cube_position(center_x, center_y, cube_len, rotation, pos)
-    local x = center_x - (cube_len / 2 * pos.x) + (cube_len / 2 * pos.z)
-    local y = center_y - (cube_len / 2 * pos.y) - (cube_len / 4 * pos.x) - (cube_len / 4 * pos.z)
+function isogen.get_cube_position(center_x, center_y, cube_len, _, pos)
+    local x = center_x -
+        (pos.x * cube_len / 2) +
+        (pos.z * cube_len / 2)
+    local y = center_y -
+        (pos.x * cube_len / 4) -
+        (pos.y * cube_len / 2) -
+        (pos.z * cube_len / 4)
+
     return x, y
 end
 
--- common.spec.lua
+++ common.spec.lua
@@ -4,7 +4,7 @@ mtt.register("calculate_image_size", function(callback)
     local cube_len = 24
     local width, height = isogen.calculate_image_size(size, cube_len)
     assert(width == 240)
-    assert(height == 132)
+    assert(height == 240)
 
     size = vector.new(1, 1, 1)
     cube_len = 24
@@ -20,7 +20,13 @@ mtt.register("get_center_cube_offset", function(callback)
     local rotation = 0
     local x, y = isogen.get_center_cube_offset(size, cube_len, rotation)
     assert(x == 108)
-    assert(y == 108)
+    assert(y == 240 - cube_len)
+
+    size = vector.new(1, 1, 1)
+    x, y = isogen.get_center_cube_offset(size, cube_len, rotation)
+    assert(x == 0)
+    assert(y == 0)
+
     callback()
 end)
 
@@ -33,19 +39,19 @@ mtt.register("get_cube_position", function(callback)
     local pos = vector.new(0, 0, 0)
     local x, y = isogen.get_cube_position(center_x, center_y, cube_len, rotation, pos)
     assert(x == 108)
-    assert(y == 108)
+    assert(y == 240 - cube_len)
 
     pos = vector.new(0, 1, 0)
     x, y = isogen.get_cube_position(center_x, center_y, cube_len, rotation, pos)
     assert(x == 108)
-    assert(y == 108-(cube_len/2))
+    assert(y == 240 - cube_len - (cube_len / 2))
 
     pos = vector.new(1, 1, 0)
     x, y = isogen.get_cube_position(center_x, center_y, cube_len, rotation, pos)
-    assert(x == 108-(cube_len/2))
-    assert(y == 108-(cube_len/2)-(cube_len/4))
+    assert(x == 108 - (cube_len / 2))
+    assert(y == 240 - cube_len - (cube_len / 2) - (cube_len / 4))
 
-    pos = vector.new(10, 10, 10)
+    pos = vector.new(9, 9, 9)
     x, y = isogen.get_cube_position(center_x, center_y, cube_len, rotation, pos)
     print(x, y)
     assert(x == 108)
