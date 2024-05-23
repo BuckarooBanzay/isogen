ee35b4e2e588166251900165ba038fa5046d5e84
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2024-05-23 09:31:49 +0200

    add `isogen.draw_map(pos1, pos2)` (#3)
    * add `isogen.draw_map(pos1, pos2)`
    
    * draw_map test
    
    ---------
    
    Co-authored-by: BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>

---
+++ draw_map.spec.lua
@@ -0,0 +1,27 @@
+local pos1 = vector.new(200,100,100)
+local pos2 = vector.add(pos1, 15)
+
+mtt.emerge_area(pos1, pos2)
+
+mtt.register("draw_map", function(callback)
+    minetest.load_area(pos1, pos2)
+
+    minetest.set_node(pos1, { name = "mapgen_stone" })
+    minetest.set_node(vector.add(pos1, vector.new(1, 0, 0)), { name = "mapgen_stone" })
+    minetest.set_node(vector.add(pos1, vector.new(2, 0, 0)), { name = "mapgen_stone" })
+    minetest.set_node(vector.add(pos1, vector.new(0, 0, 1)), { name = "mapgen_water_source" })
+    minetest.set_node(vector.add(pos1, vector.new(1, 0, 1)), { name = "mapgen_water_source" })
+
+    -- sanity tests
+    local node = minetest.get_node(pos1)
+    assert(node.name ~= "air")
+    assert(node.name ~= "ignore")
+
+    local png = isogen.draw_map(pos1, pos2)
+    local path = minetest.get_worldpath() .. "/test_map.png"
+    minetest.safe_file_write(path, png)
+
+    print("png saved: " .. #png .. " bytes")
+
+    callback()
+end)
--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ dofile(MP.."/common.lua")
 dofile(MP.."/colors.lua")
 dofile(MP.."/canvas.lua")
 dofile(MP.."/draw_cube.lua")
+dofile(MP.."/draw_map.lua")
 dofile(MP.."/draw.lua")
 
 if minetest.get_modpath("worldedit_commands") then
@@ -15,5 +16,6 @@ if minetest.get_modpath("mtt") and mtt.enabled then
     dofile(MP.."/common.spec.lua")
     dofile(MP.."/colors.spec.lua")
     dofile(MP.."/draw_cube.spec.lua")
+    dofile(MP.."/draw_map.spec.lua")
     dofile(MP.."/draw.spec.lua")
 end
